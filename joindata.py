1999df3751f229a0ab920c6be6bf26491fedc996
Emily Hess <emilynhess93@gmail.com>
2024-07-28 17:16:44 -0400

    Updated folder structure to match previeous projects

---
+++ joindata.py
@@ -0,0 +1,3 @@
+import pandas as pd
+livingcosts = pd.read_csv("C:\\Users\\emily\Documents\\Data Analytics Course\\Capstone_2024\\Cost_of_Living_Columns.csv", index_col=0)
+livingcosts.head()
--- weatherdata.py
+++
@@ -1,5 +0,0 @@
-import os;
-import pandas;
-
-#import meteosource data
-curl "https://bulk.meteostat.net/v2/hourly/10637.csv.gz" --output "10637.csv.gz";
